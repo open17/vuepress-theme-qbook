fc039d02555a7edb373f609d6913cf761c71ee8c
open17 <opensci@163.com>
2024-02-12 16:11:56 +0800

    🎈 perf(头像): 优化背景

--- packages/theme/components/UserAvatar.vue
+++ packages/theme/components/UserAvatar.vue
@@ -17,6 +17,7 @@ export default {
 </script>
 <style scoped>
 .avatar >>> .el-avatar {
+  background: #C0C4CC24;
   box-shadow: 0px 0px 10px #c3c3c3;
   transition: transform 0.5s;
   cursor: pointer;
